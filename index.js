95848f0ebd7fd17639c2508a57c25358e09290d1
ggquicky <ggquicky@gmail.com>
2022-01-25 00:14:03 -0600

    update the navbar

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var counterContainer = document.querySelector(".website-counter");
 var resetButton = document.querySelector("#reset");
 var visitCount = localStorage.getItem("page_view");
+var globalMediaQuery = window.matchMedia('(min-width:769px)');
 
 // Check if page_view entry is present
 if (visitCount) {
@@ -18,3 +19,20 @@ resetButton.addEventListener("click", () => {
   localStorage.setItem("page_view", 1);
   counterContainer.innerHTML = visitCount;
 });
+
+function openNav() {
+  document.getElementById("navbar").style.width = "100%";
+}
+if(globalMediaQuery.matches){
+  document.getElementById("navbar").style.width = "100%";
+}
+
+
+
+function closeNav() {
+  let mediaQuery = window.matchMedia('(max-width: 768px)');
+  if(mediaQuery.matches) {
+    document.getElementById("navbar").style.width = "0";
+  
+  }
+}
